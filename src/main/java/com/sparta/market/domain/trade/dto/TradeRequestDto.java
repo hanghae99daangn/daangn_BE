3fb0c9e1ffe349767fce809842c551878144fd71
Song-dyong <brandy1930@nate.com>
2024-03-20 01:37:42 +0900

    feat: Swagger 관련 설정 추가, 스키마 작성

--- src/main/java/com/sparta/market/domain/trade/dto/TradeRequestDto.java
+++ src/main/java/com/sparta/market/domain/trade/dto/TradeRequestDto.java
@@ -38,4 +38,34 @@ public class TradeRequestDto {
         }
     }
 
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    @Getter
+    public static class UpdateTradeRequestDto{
+        @Schema(description = "제목", example = "아이패드 팔아요~")
+        private String title;
+        @Schema(description = "내용", example = "5년 쓴 아이패드 팝니다! 충전기 꽂아두면 사용할 수 있어요!")
+        private String content;
+        @Schema(description = "카테고리", example = "전자기기")
+        private String category;
+        @Schema(description = "가격", example = "150000")
+        private int price;
+        @Schema(description = "만날 장소", example = "서울시 구로구 오류동")
+        private String contactPlace;
+        @Schema(description = "해당 게시글에 이미지가 있었던 경우, 그 imgId를 입력해주세요", example = "1")
+        private Long imgId;
+
+        public TradePost toEntity(User user) {
+            return TradePost.builder()
+                    .title(this.title)
+                    .content(this.content)
+                    .category(this.category)
+                    .price(this.price)
+                    .contactPlace(this.contactPlace)
+                    .user(user)
+                    .build();
+        }
+    }
+
 }

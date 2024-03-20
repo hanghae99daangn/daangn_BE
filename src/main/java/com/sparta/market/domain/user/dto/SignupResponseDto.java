72dad9fdcf14d5a15ebca4ee8bbe7ed4a25cfd7f
Song-dyong <brandy1930@nate.com>
2024-03-20 16:50:37 +0900

    feat: 회원가입 프로필 사진 추가 #2

--- src/main/java/com/sparta/market/domain/user/dto/SignupResponseDto.java
+++ src/main/java/com/sparta/market/domain/user/dto/SignupResponseDto.java
@@ -1,6 +1,7 @@
 package com.sparta.market.domain.user.dto;
 
 import com.sparta.market.domain.user.entity.User;
+import com.sparta.market.domain.user.entity.UserProfile;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +14,25 @@ public class SignupResponseDto {
     private String nickname;
     private String phoneNumber;
     private String email;
+    private Long profileId;
+    private String imageName;
+    private String s3Name;
+    private String url;
 
     public SignupResponseDto(User user) {
         this.id = user.getId();
         this.nickname = user.getNickname();
         this.phoneNumber = user.getPhoneNumber();
         this.email = user.getEmail();
+    }
+    public SignupResponseDto(User user, UserProfile userProfile) {
+        this.id = user.getId();
+        this.nickname = user.getNickname();
+        this.phoneNumber = user.getPhoneNumber();
+        this.email = user.getEmail();
+        this.profileId = userProfile.getId();
+        this.imageName = userProfile.getImageName();
+        this.s3Name = userProfile.getS3name();
+        this.url = userProfile.getUrl();
     }
 }

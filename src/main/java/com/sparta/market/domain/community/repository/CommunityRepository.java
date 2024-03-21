ddd303b5d49afccfa20ef432ffc230e18279ad5f
eleunadeu <eleunadeu@gmail.com>
2024-03-21 19:45:35 +0900

    refactor: Community Repository 미사용 import 삭제 #31

--- src/main/java/com/sparta/market/domain/community/repository/CommunityRepository.java
+++ src/main/java/com/sparta/market/domain/community/repository/CommunityRepository.java
@@ -1,12 +1,7 @@
 package com.sparta.market.domain.community.repository;
 
 import com.sparta.market.domain.community.entity.Community;
-import com.sparta.market.domain.community.entity.CommunityCategory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface CommunityRepository extends JpaRepository<Community, Long>, CommunityCustomRepository {
 }
